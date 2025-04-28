fab91eb1f65f0261583b9ea6d17199d014d04c02
faaayaaz <78535017+faaayaaz@users.noreply.github.com>
2025-04-28 15:19:35 +0400

    fix: adjust base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/fayazportfolio/' : '/', // Set base path for GitHub Pages
+export default defineConfig({
+  base: './',  // <-- THIS is the correct line for Vercel
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
     componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
